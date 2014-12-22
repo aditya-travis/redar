797aeeed16cd08d8f999d82d3543f123920a7cb4
Meng Feng <meng.feng@gbst.com>
2014-12-22 15:08:40 +0800

    Added simple custom task

---
+++ build-tools/ant-build/src/main/java/build/ant/task/CustomTask.java
@@ -0,0 +1,11 @@
+package build.ant.task;
+
+/**
+ * Created by mengf on 12/22/2014.
+ */
+public class CustomTask {
+
+    public void execute(){
+        System.out.println("This is a custom ant task..");
+    }
+}
